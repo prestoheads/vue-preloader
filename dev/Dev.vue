14da43c37b3c0f7dc2b230fbbd41f848c9eaf7a3
antalia <antalikawb@gmail.com>
2021-02-01 14:24:25 +0300

    add webpack

---
+++ dev/Dev.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-select v-model="selected" v-bind="config" />
+  </div>
+</template>
+
+<script>
+import vSelect from "../src/components/Select";
+// import countries from "../docs/.vuepress/data/countryCodes";
+// import books from "../docs/.vuepress/data/books";
+
+export default {
+  components: { vSelect },
+  data: () => ({
+    selected: null,
+    config: {
+      options: countries
+    }
+  })
+};
+</script>
---
+++ src/components/Select.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loader">
+    olololo
+  </div>
+</template>
+
+<script type="text/babel">
+  // import pointerScroll from '../mixins/pointerScroll'
+  // import typeAheadPointer from '../mixins/typeAheadPointer'
+  // import ajax from '../mixins/ajax'
+  // import childComponents from './childComponents';
+  // import appendToBody from '../directives/appendToBody';
+  // import sortAndStringify from '../utility/sortAndStringify'
+  // import uniqueId from '../utility/uniqueId';
+
+  /**
+   * @name VueSelect
+   */
+  export default {
+
+    data() {
+      return {
+      }
+    },
+    mounted() {
+      console.log('olool');
+    },
+
+    methods: {
+
+    },
+  }
+</script>
